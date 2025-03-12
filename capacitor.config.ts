acf842f4cda960a7dcad16e39e2c8fb138a594af
Joshua Enad <152776204+XeneXD@users.noreply.github.com>
2025-03-12 15:49:31 +0800

    AppPreferences
    SP2MobDev

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'AppPreference',
+  webDir: 'www'
+};
+
+export default config;
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { IonCheckbox,IonRange,IonToggle,IonRadio,IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: false
+
 })
 export class HomePage {
 
   constructor() {}
 
 }
+
+
